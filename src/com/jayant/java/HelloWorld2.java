d5e193dbed8e975f6b85ac9c61f7a51b2d408fd3
jayant <jayant.sarkar@outlook.com>
2013-12-23 13:29:29 +0530

    commit few files

---
+++ src/com/jayant/java/HelloWorld2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.jayant.java;
+
+/**
+ * @author jasarkar
+ *
+ */
+public class HelloWorld2 {
+
+	/**
+	 * 
+	 */
+	public HelloWorld2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld2");
+	}
+
+}
---
+++ src/com/jayant/java/NewSysOut.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.jayant.java;
+
+/**
+ * @author jayantsarkar
+ *
+ */
+public class NewSysOut {
+
+	/**
+	 * 
+	 */
+	public NewSysOut() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
